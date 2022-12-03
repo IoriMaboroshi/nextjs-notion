43e6b26d5ccde763ae09b0635c9f2bb7ea546ea7
伊織幻 <thechwinlyu@outlook.com>
2022-12-04 05:49:53 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '9fc4005ace274416bf5d756d02c4700a',
+  rootNotionPageId: '937b11819126413f96fba94a0f7d92ed',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: "d438f9ea-c9ef-4898-ab16-bc9242467f4e",
+  rootNotionSpaceId: "9c2f6e1b-1961-439d-9e16-911403805a5f",
 
   // basic site info (required)
-  name: 'lizhimiao的博客',
-  domain: 'lizhimiao.com',
-  author: 'zhimiao li',
+  name: '伊緻幻の主页',
+  domain: 'www.chwin.asia',
+  author: 'IoriMaboroshi',
 
   // open graph metadata (optional)
-  description: '笔记、博客和其他记录',
+  description: '伊緻幻的主页',
 
   // social usernames (optional)
-  twitter: '_lizhimiao',
-  github: 'zhimiaoli',
-  linkedin: 'lizhimiao',
+  twitter: 'IoriMaboroshi',
+  github: 'IoriMaboroshi',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -29,9 +28,6 @@ export default siteConfig({
 
   // image CDN host to proxy all image requests through (optional)
   // NOTE: this requires you to set up an external image proxy
-
-  // Utteranc.es comments via GitHub issue comments (optional)
-  //utterancesGitHubRepo: "zhimiaoli/lizhimiao-dot-com-comment",
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
@@ -47,11 +43,7 @@ export default siteConfig({
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
   pageUrlOverrides:   {
-    '/notion-as-blog': '2ac48272b7644cb0b0f8d19a104ddf05',
-    '/refers': '68892ddbd87341a5940b08fe9f3d079f',
-    '/digital-noting-tools-i-use': '7a26482a6a774a8dbd4872d9023b3957',
-    '/about': '0da3484664b44dcaa555509e3eb927f1',
-    '/archive': '299f3286fd3643ddbf6f4489b4813e81'
+    '/about': '94ff313faaf34a40bc6b4b566f5b9119',
 },
 
   // whether to use the default notion navigation style or a custom one with links to
